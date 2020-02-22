531605657242ed5836273d103badaaf762c1d2ca
petitviolet <violethero0820@gmail.com>
2020-02-22 23:17:28 +0900

    Split Rcb::Result declaration

--- lib/rcb/result.rb
+++ lib/rcb/result.rb
@@ -1,9 +1,10 @@
 require 'rstructural'
 
-module Rcb::Result
-  extend ADT
+module Rcb
+  module Result
+    extend ADT
 
-  Ok = data :new_state, :result
-  Ng = data :new_state, :error
+    Ok = data :new_state, :result
+    Ng = data :new_state, :error
+  end
 end
-
